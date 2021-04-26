db6c327f01f9b58a4cf2bb980ba34878f34c3d04
Misbah Ur Rehman <38476104+HYBRIDOITC@users.noreply.github.com>
2021-04-26 12:16:27 -0700

    Main Frame
    Main frame for the whole program. Classes functions and constructors

---
+++ task 1.cpp
@@ -0,0 +1,20 @@
+// Lab Assignment
+#include <iostream>
+using namespace std;
+
+class Sale {
+
+};
+
+Sale::Sale () {
+
+}
+
+Sale::Sale (double cost, double rate) {
+
+}
+
+int main () {
+  
+  return 0;
+}
